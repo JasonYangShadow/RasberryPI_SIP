23381edcb131972631843c06888729f8b42a2d61
jason <jasonyang.final@gmail.com>
2018-01-12 17:15:50 +0900

    update

--- src/matplot.py
+++ src/matplot.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import time
+from sqlite import Sqlite
 
-class Plot:
+class MatPlot:
 
-    def __init__(self):
-        self.__fig, (self.__ax1,self.__ax2) = plt.subplots(2,1)
+    def __init__(self,title,label_x,label_y):
+        self.__fig = plt.figure()
+        plt.title(title)
+        plt.xlabel(label_x)
+        plt.ylabel(label_y)
+        self.__x = []
+        self.__y = []
+        self.__sqlite = Sqlite()
 
+    def gen_data(self,table):
+        rows = self.__sqlite.query_delete(table)
+        for r in rows:
+            self.__x.append(r[1])
+            self.__y.append(1)
+
+    def animate(self):
+        return plt.plot(self.__x,self.__y, color ='g')
 
     def show(self):
-        plti.show()
+        anim = animation.FuncAnimation(self.__fig,self.animate,frames = self.gen_data, interval = 1000)
+        plt.show()
 
 
 if __name__ == '__main__':
-    plot = Plot() 
+    plot = MatPlot('real_time','time(s)','count')
     plot.show()
